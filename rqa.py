23abf165352561bd96bdfa1f8a18ceea5771e6b8
Felix Herter <felix.herter@pix4d.com>
2023-02-16 07:10:10 +0100

    Docu: continue module description

--- rqa.py
+++ rqa.py
@@ -1,15 +1,33 @@
 #/bin/python3
 
 # Script to rapidly assess the effect that a code change has to a wide variety
-# of test cases (QA datasets).
+# of datasets.
 #
-# It enables to compare the *new* results produced by the app with a
-# *reference* output (usually produced the current state of the app).
+# It enables to compare the results of applying a test version of the binary
+# to a set of QA datasets against the results produced by a reference version.
 #
-# Notes: The terms QA datasets and QA projects are used interchangeably and
-# refer, depending on the context, to the set of images that belong to one
-# project or the folder containing all information for the project, including
-# the images).
+# Terminology:
+# *Test case* refers to the artifacts obtained by applying a specific binary
+# version to a specific QA dataset.
+# *Test suite* refers to the set of test cases obtained from applying a specific
+# binary version to all QA datasets.
+# *QA project* refers to the folder containing a QA dataset and possibly
+# related data.
+# The *test suite identifier* is a sha1 and an additional index. The sha1
+# identifies the binary version and the additional index allows to distinguish
+# binaries that were generated form the same commit in a dirty repo
+# (uncommitted changes).
+#
+# A folder for a test case is named in the following way:
+#
+#    <sha1>_<id>_<qaDatasetName>_<optionalDescription>
+#
+# <sha1> and <id> together form the test suite identifier, <qaDatasetName> is
+# the name of the dataset converted to camel case, and <optionalDescription> is
+# an optional description supplied by the user to make it easier to relate the
+# test suits to the change that they test, e.g.,
+#
+#    1234567890_001_snowyHillside_increasedStepSizeTo42
 
 import sys
 import os
