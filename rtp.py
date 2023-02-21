757e87dee6c7c87f868748a237b5fe78188d6c75
Felix Herter <felix.herter@pix4d.com>
2023-02-21 13:49:08 +0100

    Add: test that call_test_pipeline() emits the right call

--- rtp.py
+++ rtp.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+import rqa
+
+import logging
 import subprocess
 from argparse import ArgumentParser
 from pathlib import Path
@@ -13,16 +16,33 @@ def execute_command(command: list[str]):
     subprocess_result = subprocess.run(command, capture_output=True, shell=True)
     msg = subprocess_result.stdout
     msg = msg.decode('utf-8').strip()
-    print(f'msg is: {msg}')
     try:
         subprocess_result.check_returncode()
         return msg
     except subprocess.CalledProcessError:
         raise RuntimeError(f"failed executing binary '{command.split()[0]} ...':\n'{msg}'")
 
-if __name__ == '__main__':
-    images = [str(image_path) for image_path in data_path.glob('*')]
-    out_args = ['-o', str(out_path / '02_')]
 
-    result = execute_command(' '.join([binary] + out_args + images))
-    print("result is: ", result)
+def test_pipeline(app_path: Path,
+                  out_path: Path,
+                  config_path: Path,
+                  images_path: Path):
+    command = str(app_path)
+    command += ' -f ' + str(config_path)
+    command += ' -o ' + str(out_path)
+    command += ' ' + ' '.join(str(image_path) for image_path in images_path.glob('*'))
+    return execute_command(command)
+
+if __name__ == '__main__':
+    binary_path = Path(binary).expanduser()
+    repo = rqa.Repo(Path(binary_path))
+
+    if not out_path.exists():
+        logging.info(f"output directory '{out_path}' not found, creating it")
+        out_path.mkdir(parents=True)
+
+    #images = [str(image_path) for image_path in data_path.glob('*')]
+    #out_args = ['-o', str(out_path / '02_')]
+
+    #result = execute_command(' '.join([binary] + out_args + images))
+    #print("result is: ", result)
--- test_helpers.py
+++ test_helpers.py
@@ -1,9 +1,9 @@
 import helpers
+import rtp
 
+import os
 import pytest
-import shutil
 import subprocess
-import time
 from pathlib import Path
 
 # TODO: test this
@@ -30,6 +30,43 @@ def tmp_repo(tmp_path):
     return tmp_path
 
 
+echo_call_program = "echo $0 $@"
+
+@pytest.fixture
+def repo_with_call_inspection_executable(tmp_path, dummy_test_pipeline=echo_call_program):
+    """Fixture that yields a repo with an executable."""
+    executable_path = tmp_path / "app"
+    with open(executable_path, "w") as f:
+        f.write(dummy_test_pipeline)
+    os.chmod(executable_path, 0o700) # owner may read, write, or execute
+    yield {'repo': tmp_path, 'executable': executable_path}
+
+
+@pytest.fixture
+def testpipeline_environment(repo_with_call_inspection_executable):
+    """Fixture that yields a complete environment for test_pipeline.
+
+    This comprises
+     * `repo_path`   path to a git repo
+     * `app_path`    path to a dummy test_pipeline app inside `repo`for introspection
+     * `images_path` path to a folder with dummy images
+     * `config_path` dummy path to a config file
+     * `out_path`    dummy path to an output folder
+    """
+    repo_path = repo_with_call_inspection_executable['repo']
+    app_path = repo_with_call_inspection_executable['executable']
+    images_path = repo_path.parent / 'Images'
+    images_path.mkdir()
+    for img in ['img_01.TIF', 'img_02.TIF']:
+        (images_path / img).touch()
+    config_path = 'dummy/config/path'
+    out_path = 'dummy/out/path'
+    return {'repo_path': repo_path,
+            'app_path': app_path,
+            'images_path': images_path,
+            'config_path': config_path,
+            'out_path': out_path}
+
 #----------------------------------------------------------------------test Repo
 def test_repo_class_can_be_constructed_from_repo_path(tmp_repo):
     helpers.Repo(tmp_repo)
@@ -38,3 +75,20 @@ def test_repo_class_can_be_constructed_from_repo_path(tmp_repo):
 def test_repo_class_cannot_be_constructed_from_non_repo_path(tmp_path):
     with pytest.raises(helpers.Repo.NotARepoException):
         helpers.Repo(tmp_path)
+
+
+#-----------------------------------------------------------------------test rtp
+def test_call_test_pipeline_executes_the_expected_command(testpipeline_environment):
+    app_path = testpipeline_environment['app_path']
+    images_path = testpipeline_environment['images_path']
+    out_path = testpipeline_environment['out_path']
+    config_path = testpipeline_environment['config_path']
+
+    command_triggered = rtp.test_pipeline(app_path=app_path,
+                                          out_path=out_path,
+                                          config_path=config_path,
+                                          images_path=images_path)
+
+    expected_command = (f'{app_path} -f {config_path} -o {out_path} ' +
+                         ' '.join(str(image_path) for image_path in images_path.glob('*')))
+    assert command_triggered == expected_command
