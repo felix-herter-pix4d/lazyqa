3c5e70b6a38e064d64876a82c6aa65bb84e7b38f
Felix Herter <felix.herter@pix4d.com>
2023-03-20 09:09:03 +0100

    Refactor: move tests to test_lazytp

--- test_common.py
+++ test_common.py
@@ -145,38 +145,6 @@ def test_create_input_block_for_config_returns_correct_block(environment_for_tes
 
 
 #-----------------------------------------------------------------------test ltp
-def test_lazy_test_pipeline_creates_correct_output_folder_when_no_description_is_given(environment_for_test_pipeline):
-    env = environment_for_test_pipeline
-
-    ltp.lazy_test_pipeline(app_path = env['app_path'],
-                           out_root_path = env['out_path'],
-                           images_path = env['images_path'],
-                           config_path = env['config_path'])
-
-    expected_qa_project_name = common.camel_case(env['images_path'].parent.name)
-    expected_id = '004' # previous highest id was 003
-    all_correct_output_folders = env['out_path'].glob(f"{expected_id}*{expected_qa_project_name}")
-    assert len(list(all_correct_output_folders)) == 1
-
-
-def test_lazy_test_pipeline_creates_correct_output_folder_when_description_is_given(environment_for_test_pipeline):
-    env = environment_for_test_pipeline
-    description = "this is a test case"
-
-    ltp.lazy_test_pipeline(app_path = env['app_path'],
-                           out_root_path = env['out_path'],
-                           optional_description = description,
-                           images_path = env['images_path'],
-                           config_path = env['config_path'])
-
-    expected_qa_project_name = common.camel_case(env['images_path'].parent.name)
-    expected_description = common.camel_case(description)
-    expected_id = '004' # previous highest id was 003
-    all_correct_output_folders = env['out_path'].glob(
-        f"{expected_id}*{expected_qa_project_name}*{expected_description}")
-    assert len(list(all_correct_output_folders)) == 1
-
-
 def test_lazy_test_pipeline_writes_log_to_qa_test_case_folder(environment_for_test_pipeline):
     env = environment_for_test_pipeline
     collect_current_qa_test_cases = lambda : set(env['out_path'].glob('*'))
--- test_lazytp.py
+++ test_lazytp.py
@@ -1,4 +1,5 @@
 import lazytp as ltp
+import common
 from test_helpers import *
 
 import re
@@ -43,4 +44,36 @@ def test_lazy_test_pipeline_reads_local_config(environment_for_test_pipeline):
     get_config_argument = lambda call : re.match(r'.*-f (\S*).*', call).group(1) # capture argument after '-f' flag
     config_used_path = get_config_argument(what_was_called)
     assert content_of(env['config_path']) in content_of(config_used_path) # not equal, copy is enriched
+
+
+def test_lazy_test_pipeline_creates_correct_output_folder_when_no_description_is_given(environment_for_test_pipeline):
+    env = environment_for_test_pipeline
+
+    ltp.lazy_test_pipeline(app_path = env['app_path'],
+                           out_root_path = env['out_path'],
+                           images_path = env['images_path'],
+                           config_path = env['config_path'])
+
+    expected_qa_project_name = common.camel_case(env['images_path'].parent.name)
+    expected_id = '004' # previous highest id was 003
+    all_correct_output_folders = env['out_path'].glob(f"{expected_id}*{expected_qa_project_name}")
+    assert len(list(all_correct_output_folders)) == 1
+
+
+def test_lazy_test_pipeline_creates_correct_output_folder_when_description_is_given(environment_for_test_pipeline):
+    env = environment_for_test_pipeline
+    description = "this is a test case"
+
+    ltp.lazy_test_pipeline(app_path = env['app_path'],
+                           out_root_path = env['out_path'],
+                           optional_description = description,
+                           images_path = env['images_path'],
+                           config_path = env['config_path'])
+
+    expected_qa_project_name = common.camel_case(env['images_path'].parent.name)
+    expected_description = common.camel_case(description)
+    expected_id = '004' # previous highest id was 003
+    all_correct_output_folders = env['out_path'].glob(
+        f"{expected_id}*{expected_qa_project_name}*{expected_description}")
+    assert len(list(all_correct_output_folders)) == 1
 
